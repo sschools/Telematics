e2f22cb571fe3656c4939c89af593ed633b9513d
Stephen Schools <stephen.schools>
2017-09-19 15:12:35 -0400

    writing to a file working

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -1,8 +1,35 @@
 package com.ironyard;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws JsonProcessingException {
+        Scanner scanner = new Scanner(System.in);
+        VehicleInfo myCar = new VehicleInfo();
+
+        System.out.println("Enter VIN: ");
+        Integer vin = scanner.nextInt();
+        myCar.setVIN(vin);
+
+        System.out.println("Odometer reading: ");
+        Double odom = scanner.nextDouble();
+        myCar.setOdometer(odom);
+
+        System.out.println("Gallons of fuel consumed: ");
+        Double gas = scanner.nextDouble();
+        myCar.setConsumption(gas);
+
+        System.out.println("Miles since last oil change: ");
+        Double oil = scanner.nextDouble();
+        myCar.setMilesSinceOil(oil);
+
+        System.out.println("Engine size (liters): ");
+        Double liters = scanner.nextDouble();
+        myCar.setEngineSize(liters);
+
+        TelematicsService.report(myCar);
     }
 }
--- src/com/ironyard/TelematicsService.java
+++ src/com/ironyard/TelematicsService.java
@@ -1,5 +1,26 @@
 package com.ironyard;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TelematicsService {
-    public void report(VehicleInfo vehicleInfo);
+    public static void report(VehicleInfo vehicleInfo) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(vehicleInfo);
+        System.out.println(json);
+        String title = vehicleInfo.getVIN() + ".json";
+        try {
+            File file = new File(title);
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(json);
+            fileWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    };
+
 }
